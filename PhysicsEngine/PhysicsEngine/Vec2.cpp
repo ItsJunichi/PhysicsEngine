53a0efae014497eecedccd86e56c2ce45904e6c8
Juuso Murto <juusomurto@kamk.fi>
2020-03-30 12:26:36 +0300

    created definitions for vec2 in cpp instead of header for clarity

--- PhysicsEngine/PhysicsEngine/Vec2.cpp
+++ PhysicsEngine/PhysicsEngine/Vec2.cpp
@@ -8,6 +8,26 @@ Vec2::Vec2(float PosX, float PosY){
 
 }
 
+Vec2 Vec2::operator+(const Vec2& smh)
+{
+	return Vec2(this->x + smh.x, this->y + smh.y);
+}
+
+Vec2 Vec2::operator-(const Vec2& smh)
+{
+	return Vec2(this->x - smh.x, this->y - smh.y);
+}
+
+Vec2 Vec2::operator*(const Vec2& smh)
+{
+	return Vec2(this->x * smh.x, this->y * smh.y);
+}
+
+Vec2 Vec2::operator/(const Vec2& smh)
+{
+	return Vec2(this->x / smh.x, this->y / smh.y);
+}
+
 Vec2::Vec2() {
 	x = 0.0f;
 	y = 0.0f;
--- PhysicsEngine/PhysicsEngine/Vec2.h
+++ PhysicsEngine/PhysicsEngine/Vec2.h
@@ -9,25 +9,14 @@ public:
 	~Vec2();
 	Vec2(float PosX, float PosY);
 
-	//TODO: 
-	//Vec2 Operators
+	Vec2 operator+(const Vec2& smh);
 
-	
-	Vec2 operator+(const Vec2& smh) const {
-		return Vec2(this->x + smh.x, this->y + smh.y);
-	}
+	Vec2 operator-(const Vec2& smh);
 
-	Vec2 operator-(const Vec2 &smh) const {
-		return Vec2(this->x - smh.x, this->y - smh.y);
-	}
+	Vec2 operator*(const Vec2& smh);
 
-	Vec2 operator*(const Vec2& smh) const {
-		return Vec2(this->x * smh.x, this->y * smh.y);
-	}
-
-	Vec2 operator/(const Vec2& smh) const {
-		return Vec2(this->x / smh.x, this->y / smh.y);
-	}
+	Vec2 operator/(const Vec2& smh);
+		
 	
 };
 
